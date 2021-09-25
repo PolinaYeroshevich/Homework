f086211d526a25dcfd6ff7e2dd921af594fec06a
Polina Yeroshevich <polinayeroshevich99@gmail.com>
2021-09-25 22:57:45 +0300

    ready-made solution

---
+++ task_DEV_1/task_DEV_1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace task_DEV_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter A Number :");
+            int index = int.Parse(Console.ReadLine());
+            if (index % 2 != 0 && index % 5 != 0 && (index % 2 != 0 || index % 5 != 0))
+            { 
+                return;
+            }
+            if (index % 2 == 0 && index % 5 == 0)
+            {
+                Console.WriteLine("Tutti-Frutti");
+            }
+            else if (index % 2 == 0)
+            {
+                Console.WriteLine("Tutti");
+            }
+            else if (index % 5 == 0)
+            {
+                Console.WriteLine("Frutti");
+            }
+        
+        }
+    }
+            
+        }
+                
+       
+
+           
+        
+

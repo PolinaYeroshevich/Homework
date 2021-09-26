9c2a91c2d0fcb2a31dd240588ca5d6f2e7f07c0c
Polina Yeroshevich <polinayeroshevich99@gmail.com>
2021-09-26 19:29:05 +0300

    third bug fix

--- task_DEV_1/task_DEV_1/Program.cs
+++ task_DEV_1/task_DEV_1/Program.cs
@@ -21,7 +21,8 @@ namespace task_DEV_1
             {
                 Console.WriteLine("Frutti");
             }
-            else (!(inputNumber % 2 == 0) && !(inputNumber % 5 == 0))
+            else 
+                //(!(inputNumber % 2 == 0) && !(inputNumber % 5 == 0))
             {
                 Console.WriteLine("This number is not divisible by 2 and 5");
             }

3f80326574305fc176700796bffc8ea89861589c
Polina Yeroshevich <polinayeroshevich99@gmail.com>
2021-09-30 22:13:43 +0300

    creating a cycle with division by 2 and 5

--- task_DEV_1/task_DEV_1/Program.cs
+++ task_DEV_1/task_DEV_1/Program.cs
@@ -8,24 +8,32 @@ namespace task_DEV_1
         {
             Console.WriteLine("Enter A Number :");
             int inputNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter A Number :");
+            int secondInputNumber = int.Parse(Console.ReadLine());
 
-            if (inputNumber % 2 == 0 && inputNumber % 5 == 0)
+            for (; inputNumber <= secondInputNumber; inputNumber++)
             {
-                Console.WriteLine("Tutti-Frutti");
-            }
-            else if (inputNumber % 2 == 0)
-            {
-                Console.WriteLine("Tutti");
-            }
-            else if (inputNumber % 5 == 0)
-            {
-                Console.WriteLine("Frutti");
-            }
-            else 
-                //(!(inputNumber % 2 == 0) && !(inputNumber % 5 == 0))
-            {
-                Console.WriteLine("This number is not divisible by 2 and 5");
+                if (inputNumber % 2 == 0 && inputNumber % 5 == 0)
+                {
+                    Console.WriteLine("Tutti-Frutti");
+                }
+
+                else if (inputNumber % 2 == 0)
+
+                {
+                    Console.WriteLine("Tutti");
+                }
+
+                else if (inputNumber % 5 == 0)
+                {
+                    Console.WriteLine("Frutti");
+                }
+
+                else
+                {                    
+                    Console.WriteLine($"Number {inputNumber} can't be divided on 2 or 5");
+                }
             }
         }
-    }            
-}        
+    }
+}

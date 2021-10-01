d36be231331d78ae129d5895acaf37d5e8797165
Polina Yeroshevich <polinayeroshevich99@gmail.com>
2021-10-01 20:20:21 +0300

    Added variable, removed blank lines

--- task_DEV_1/task_DEV_1/Program.cs
+++ task_DEV_1/task_DEV_1/Program.cs
@@ -7,31 +7,27 @@ namespace task_DEV_1
         static void Main(string[] args)
         {
             Console.WriteLine("Enter A Number :");
-            int inputNumber = int.Parse(Console.ReadLine());
+            int firstInputNumber = int.Parse(Console.ReadLine());
             Console.WriteLine("Enter A Number :");
             int secondInputNumber = int.Parse(Console.ReadLine());
 
-            for (; inputNumber <= secondInputNumber; inputNumber++)
+            for (int i=firstInputNumber; i <= secondInputNumber ;i++)
             {
-                if (inputNumber % 2 == 0 && inputNumber % 5 == 0)
+                if (i % 2 == 0 && i % 5 == 0)
                 {
                     Console.WriteLine("Tutti-Frutti");
                 }
-
-                else if (inputNumber % 2 == 0)
-
+                else if (i % 2 == 0)
                 {
                     Console.WriteLine("Tutti");
                 }
-
-                else if (inputNumber % 5 == 0)
+                else if (i % 5 == 0)
                 {
                     Console.WriteLine("Frutti");
                 }
-
                 else
                 {                    
-                    Console.WriteLine($"Number {inputNumber} can't be divided on 2 or 5");
+                    Console.WriteLine($"Number {i} can't be divided on 2 or 5");
                 }
             }
         }

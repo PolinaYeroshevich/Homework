31c1555572c3561d7d638dc73b51c7b1ff89a9b1
Polina Yeroshevich <polinayeroshevich99@gmail.com>
2021-10-05 17:12:49 +0300

    Reverse division added

--- task_DEV_1/task_DEV_1/Program.cs
+++ task_DEV_1/task_DEV_1/Program.cs
@@ -12,7 +12,7 @@ namespace task_DEV_1
             int secondInputNumber = int.Parse(Console.ReadLine());
 
             for (int i = firstInputNumber; i <= secondInputNumber; i++)
-            {
+                {
                 if (i % 2 == 0 && i % 5 == 0)
                 {
                     Console.WriteLine("Tutti-Frutti");
@@ -27,6 +27,25 @@ namespace task_DEV_1
                 }
                 else
                 {                    
+                    Console.WriteLine($"Number {i} can't be divided on 2 or 5");
+                }
+            }
+            for (int i = firstInputNumber; i >= secondInputNumber; i--)
+            {
+                if (i % 2 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("Tutti-Frutti");
+                }
+                else if (i % 2 == 0)
+                {
+                    Console.WriteLine("Tutti");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Frutti");
+                }
+                else
+                {
                     Console.WriteLine($"Number {i} can't be divided on 2 or 5");
                 }
             }

3d9752ba78bbacee8963de34b1f64078ecbc8ce6
Polina Yeroshevich <polinayeroshevich99@gmail.com>
2021-10-21 22:47:46 +0300

    task-DEV-1

---
+++ Task_DEV_2/Task_DEV_2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Task_DEV_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the size of the array :");
+            int arrayLength = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < arrayLength;)
+            {
+                if (arrayLength > 20)
+
+                    break;
+
+                Console.WriteLine("Array cannot be <= 20");
+            }
+            int[] inputNumbers = new int[arrayLength];
+
+            for (int i = 0; i < arrayLength; i++)
+            {
+                Console.WriteLine($"Enter an array element № {i}");
+                inputNumbers[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            int min = inputNumbers[0];
+            int max = inputNumbers[0];
+            int indexMin = 0;
+            int indexMax = 0;
+
+            for (int i = 0; i < arrayLength; i++)
+            {
+                if (inputNumbers[i] < min)
+                {
+                    min = inputNumbers[i];
+                    indexMin = i;
+                }
+                if (inputNumbers[i] > max)
+                {
+                    max = inputNumbers[i];
+                    indexMax = i;
+                }
+                Console.WriteLine(inputNumbers[i]);
+            }
+            inputNumbers[indexMin] = max;
+            inputNumbers[indexMax] = min;
+
+            for (int i = 0; i < arrayLength; i++)
+            {
+                Console.WriteLine(inputNumbers[i]);
+            }
+        }
+    }
+}
---
+++ task_DEV_1/task_DEV_1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace task_DEV_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the first number");
+            int firstInputNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the second number");
+            int secondInputNumber = int.Parse(Console.ReadLine());
+            int min = 0;
+            int max = 0;
+
+            if (firstInputNumber <= secondInputNumber)
+            {
+                min = firstInputNumber;
+                max = secondInputNumber;
+            }
+            else 
+            {
+                min = secondInputNumber;
+                max = firstInputNumber;
+            }
+            for (int i = min; i <= max; i++)
+            {
+                if (i % 2 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("Tutti-Frutti");
+                }
+                else if (i % 2 == 0)
+                {
+                    Console.WriteLine("Tutti");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Frutti");
+                }
+                else
+                {
+                    Console.WriteLine($"Number {i} can't be divided on 2 or 5");
+                }
+            }
+        }
+    }
+}

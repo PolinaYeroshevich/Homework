0a2079b85050c29eb44469782a31d3c776d88888
Polina Yeroshevich <polinayeroshevich99@gmail.com>
2021-11-02 22:15:10 +0300

    Finding min and max of an array using methods

--- Task_DEV_2/Task_DEV_2/Program.cs
+++ Task_DEV_2/Task_DEV_2/Program.cs
@@ -4,46 +4,59 @@ namespace Task_DEV_2
 {
     class Program
     {
+        static void InputArrayElement(int[] inputNumbers, int arrayLength)
+        {
+            for (int i = 0; i < arrayLength; i++)
+            {
+                Console.WriteLine($"Enter an array element № {i}");
+                inputNumbers[i] = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+        static void ChangeMinMaxElement(int[] inputNumbers, int arrayLength)
+        {
+            int min = inputNumbers[0];
+            int max = inputNumbers[0];
+            int indexMin = 0;
+            int indexMax = 0;
+
+            for (int i = 0; i < arrayLength; i++)
+            {
+                if (inputNumbers[i] < min)
+                {
+                    min = inputNumbers[i];
+                    indexMin = i;
+                }
+                if (inputNumbers[i] > max)
+                {
+                    max = inputNumbers[i];
+                    indexMax = i;
+                }
+                Console.WriteLine(inputNumbers[i]);
+            }
+            inputNumbers[indexMin] = max;
+            inputNumbers[indexMax] = min;
+        }
+        static void OutputArray(int[] inputNumbers, int arrayLength)
+        {
+            for (int i = 0; i < arrayLength; i++)
+            {
+                Console.WriteLine(inputNumbers[i]);
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter the size of the array :");
+            Console.WriteLine("Enter the length of the array :");
             int arrayLength = Convert.ToInt32(Console.ReadLine());
 
             if (arrayLength <= 20)
             {
                 int[] inputNumbers = new int[arrayLength];
 
-                for (int i = 0; i < arrayLength; i++)
-                {
-                    Console.WriteLine($"Enter an array element № {i}");
-                    inputNumbers[i] = Convert.ToInt32(Console.ReadLine());
-                }
-                int min = inputNumbers[0];
-                int max = inputNumbers[0];
-                int indexMin = 0;
-                int indexMax = 0;
+                InputArrayElement(inputNumbers, arrayLength);
 
-                for (int i = 0; i < arrayLength; i++)
-                {
-                    if (inputNumbers[i] < min)
-                    {
-                        min = inputNumbers[i];
-                        indexMin = i;
-                    }
-                    if (inputNumbers[i] > max)
-                    {
-                        max = inputNumbers[i];
-                        indexMax = i;
-                    }
-                    Console.WriteLine(inputNumbers[i]);
-                }
-                inputNumbers[indexMin] = max;
-                inputNumbers[indexMax] = min;
+                ChangeMinMaxElement(inputNumbers, arrayLength);
 
-                for (int i = 0; i < arrayLength; i++)
-                {
-                    Console.WriteLine(inputNumbers[i]);
-                }
+                OutputArray(inputNumbers, arrayLength);
             }
             else
             {

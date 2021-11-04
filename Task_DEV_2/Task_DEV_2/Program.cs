005eb7002a47bae4a75b7820b86acb7271e88a6f
Polina Yeroshevich <polinayeroshevich99@gmail.com>
2021-11-04 21:32:57 +0300

    Fixed code style, renamed and fixed methods

--- Task_DEV_2/Task_DEV_2/Program.cs
+++ Task_DEV_2/Task_DEV_2/Program.cs
@@ -4,15 +4,18 @@ namespace Task_DEV_2
 {
     class Program
     {
-        static void InputArrayElement(int[] inputNumbers, int arrayLength)
+        static int[] CreateNumbersArray(int arrayLength)
         {
+            int[] inputNumbers = new int[arrayLength];
             for (int i = 0; i < arrayLength; i++)
             {
                 Console.WriteLine($"Enter an array element № {i}");
                 inputNumbers[i] = Convert.ToInt32(Console.ReadLine());
             }
+            return inputNumbers;
         }
-        static void ChangeMinMaxElement(int[] inputNumbers, int arrayLength)
+
+        static int SwapMinMaxElements(int[] inputNumbers, int arrayLength)
         {
             int min = inputNumbers[0];
             int max = inputNumbers[0];
@@ -31,32 +34,33 @@ namespace Task_DEV_2
                     max = inputNumbers[i];
                     indexMax = i;
                 }
-                Console.WriteLine(inputNumbers[i]);
             }
             inputNumbers[indexMin] = max;
             inputNumbers[indexMax] = min;
+
+            return inputNumbers[indexMin];
         }
-        static void OutputArray(int[] inputNumbers, int arrayLength)
+
+        static void PrintNumberArraryToConsole(int[] inputNumbers, int arrayLength)
         {
+
             for (int i = 0; i < arrayLength; i++)
             {
                 Console.WriteLine(inputNumbers[i]);
             }
         }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter the length of the array :");
+            Console.WriteLine("Enter the size of the array :");
             int arrayLength = Convert.ToInt32(Console.ReadLine());
 
             if (arrayLength <= 20)
             {
-                int[] inputNumbers = new int[arrayLength];
-
-                InputArrayElement(inputNumbers, arrayLength);
-
-                ChangeMinMaxElement(inputNumbers, arrayLength);
-
-                OutputArray(inputNumbers, arrayLength);
+                int[] inputNumbers = CreateNumbersArray(arrayLength);
+                PrintNumberArraryToConsole(inputNumbers, arrayLength);
+                SwapMinMaxElements(inputNumbers, arrayLength);
+                PrintNumberArraryToConsole(inputNumbers, arrayLength);
             }
             else
             {

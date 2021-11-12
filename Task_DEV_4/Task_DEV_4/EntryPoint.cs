b4bdc0290b2f905393d0448fe6e807089e2dc939
Polina Yeroshevich <polinayeroshevich99@gmail.com>
2021-11-12 19:49:31 +0300

    Created class and methods rectangle

---
+++ Task_DEV_4/Task_DEV_4/EntryPoint.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Task_DEV_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rectangle = new Rectangle ();
+            double length = 4;
+            double width = 7;
+            rectangle.CreateLengthAndWidth(length,width);
+            double rectangleArea = rectangle.FindArea();
+            Console.WriteLine($"The area of a rectangle is {rectangleArea}");
+            double rectanglePerimetr = rectangle.FindPerimetr();
+            Console.WriteLine($"The perimetr of a rectangle is {rectanglePerimetr}");
+            bool isNotSquare = rectangle.CheckForSquare();
+        }
+    }
+}
---
+++ Task_DEV_4/Task_DEV_4/Rectangle.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+
+namespace Task_DEV_4
+{
+    class Rectangle
+    {
+        private double length;
+        private double width;
+
+        public Rectangle()
+        {
+
+        }       
+
+        public Rectangle(double length, double width)
+        {
+            this.length = length;
+            this.width = width;
+        }
+
+        public void CreateLengthAndWidth(double length, double width)
+        {
+            this.length = length;
+            this.width = width;
+        }
+
+        public double FindArea()
+        {
+            return length * width;
+        }
+
+        public double FindPerimetr()
+        {
+            return (length + width) * 2;
+        }
+        
+        public bool CheckForSquare ()
+        {
+            bool isNotSquare = true;
+            if (length == width)
+            {
+                isNotSquare = false;
+                Console.WriteLine("It's a square");
+            }
+            else
+            {
+                Console.WriteLine("It's not a square");
+            }
+            return isNotSquare;
+        }
+    }
+}
+

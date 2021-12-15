86fcb8e782392fb7a6f8fecc13eb06a8492bf7b4
Polina Yeroshevich <polinayeroshevich99@gmail.com>
2021-12-15 20:44:21 +0300

    Classes that sort the text alphabetically have been created

---
+++ Task_DEV_5/Task_DEV_5/EntryPoint.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace task_DEV_5
+{
+    class EntryPoint
+    {
+        static void Main(string[] args)
+        {
+            //FirstTextSorter firstTextSorter = new FirstTextSorter(@"D:\Text.txt", @"D:\Text.txt");
+            //firstTextSorter.ReadText();
+            //firstTextSorter.SortText();
+            //firstTextSorter.WriteText();
+
+            SecondTextSort secondTextSort = new SecondTextSort(@"D:\Text.txt", @"D:\Text.txt");
+            secondTextSort.ReadText();
+            secondTextSort.WriteText();
+        }
+    }
+}

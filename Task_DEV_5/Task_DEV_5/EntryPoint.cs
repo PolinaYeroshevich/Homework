d160d0f8959f56063d5cb1a90185c39d95234e7e
Polina Yeroshevich <polinayeroshevich99@gmail.com>
2022-02-07 21:46:47 +0300

    Created character-by-character text sorter and random text generation.

--- Task_DEV_5/Task_DEV_5/EntryPoint.cs
+++ Task_DEV_5/Task_DEV_5/EntryPoint.cs
@@ -1,20 +1,34 @@
 ﻿using System;
 using System.IO;
-
 namespace task_DEV_5
 {
     class EntryPoint
     {
         static void Main(string[] args)
         {
-            //FirstTextSorter firstTextSorter = new FirstTextSorter(@"D:\Text.txt", @"D:\Text.txt");
-            //firstTextSorter.ReadText();
-            //firstTextSorter.SortText();
-            //firstTextSorter.WriteText();
+            try
+            {
+                try
+                {
+                    Console.WriteLine("Enter the number of lines");
+                    int number = int.Parse(Console.ReadLine());
+                    FileGeneration fileGeneration = new FileGeneration(@"D:\git\Homework\Task_DEV_5\Text.txt", number);
+                    fileGeneration.WriteText();
+                }
+                catch (ArgumentOutOfRangeException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                FirstTextSorter firstTextSorter = new FirstTextSorter(@"D:\git\Homework\Task_DEV_5\Text1.txt");
+                firstTextSorter.SortText();
 
-            SecondTextSort secondTextSort = new SecondTextSort(@"D:\Text.txt", @"D:\Text.txt");
-            secondTextSort.ReadText();
-            secondTextSort.WriteText();
+                SecondTextSort secondTextSort = new SecondTextSort(@"D:\git\Homework\Task_DEV_5\Text2.txt");
+                secondTextSort.SortText();
+            }
+            catch (DirectoryNotFoundException)
+            {
+                Console.WriteLine("The file path is not valid");
+            }
         }
     }
 }
---
+++ Task_DEV_5/Task_DEV_5/FirstSorter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace task_DEV_5
+{
+    class FirstTextSorter
+    {
+        public string path;
+
+        public FirstTextSorter(string path)
+        {
+            this.path = path;
+        }
+
+        public void SortText()
+        {
+            string[] contents = File.ReadAllLines(path);
+            Array.Sort(contents);
+            File.WriteAllLines(path, contents);
+        }
+    }
+}

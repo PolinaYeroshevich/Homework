efdfcb1e76e9bf721f77da7bddeed7d442b4308e
Polina Yeroshevich <polinayeroshevich99@gmail.com>
2022-02-23 19:38:42 +0300

    Created a stack based on an array

---
+++ Task_DEV_6/Task_DEV_6/CustomStack.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Task_DEV_6
+{
+    class CustomStack
+    {
+        public int[] numbers;
+        public int count;
+
+        public CustomStack()
+        {
+            numbers = new int[10];
+        }
+
+        public CustomStack(int capacity)
+        {
+            numbers = new int[capacity];
+        }
+
+        public int Capasity()
+        {
+            return numbers.Length;
+        }
+
+        public int Count()
+        {
+            return count;
+        }
+
+        public bool isEmpty()
+        {
+            if (count == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public int Push(int number)
+        {
+            if (count == numbers.Length)
+            {
+                Array.Resize(ref numbers, numbers.Length + numbers.Length);
+            }
+            return numbers[count++] = number;
+        }
+
+        public int Pop()
+        {
+            if (count == numbers.Length / 2)
+            {
+                Array.Resize(ref numbers, numbers.Length / 2);
+            }
+            return numbers[--count];
+        }
+    }
+}
---
+++ Task_DEV_6/Task_DEV_6/EntryPoint.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Task_DEV_6
+{
+    class EntryPoint
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                CustomStack customStack = new CustomStack();
+                int capasity = customStack.Capasity();
+                int count = customStack.Count();
+                customStack.Push(1);
+                customStack.Push(7);
+                customStack.Push(8);
+                bool isEmpty = customStack.isEmpty();
+                count = customStack.Count();
+                int number = customStack.Pop();
+                number = customStack.Pop();
+                number = customStack.Pop();
+                count = customStack.Count();
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("There are no elements on the stack.");
+            }
+        }
+    }
+}
